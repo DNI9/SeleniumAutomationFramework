6d33c3ee749280aa444e79aedd91a5a380f67e95
DNI9 <itsr9boi@gmail.com>
2022-11-28 17:28:43 +0530

    use different config for different environments

---
+++ src/test/java/org/dni9/pom/constants/EnvType.java
@@ -0,0 +1,7 @@
+package org.dni9.pom.constants;
+
+public enum EnvType {
+  PRODUCTION,
+  STAGING,
+  DEVELOPMENT
+}
--- src/test/java/org/dni9/pom/utils/ConfigLoader.java
+++ src/test/java/org/dni9/pom/utils/ConfigLoader.java
@@ -3,22 +3,43 @@ package org.dni9.pom.utils;
 import java.util.Objects;
 import java.util.Properties;
 
+import org.dni9.pom.constants.EnvType;
+
 public class ConfigLoader {
   private static ConfigLoader configLoader;
   private final Properties properties;
 
   private ConfigLoader() {
-    properties = PropertyUtils.propertyLoader("src/test/resources/config.properties");
+    String env = System.getProperty("env", EnvType.STAGING.toString());
+    String configFile = "";
+
+    switch (EnvType.valueOf(env)) {
+      case STAGING:
+        configFile = "stg_config.properties";
+        break;
+
+      case PRODUCTION:
+        configFile = "prod_config.properties";
+        break;
+
+      default:
+        throw new IllegalArgumentException("Config file for " + env + " not found");
+    }
+
+    System.out.println("INFO: Using config file for " + EnvType.valueOf(env) + " environment");
+    properties = PropertyUtils.propertyLoader("src/test/resources/" + configFile);
   }
 
   public static ConfigLoader getInstance() {
-    if (Objects.isNull(configLoader)) configLoader = new ConfigLoader();
+    if (Objects.isNull(configLoader))
+      configLoader = new ConfigLoader();
     return configLoader;
   }
 
   public String getConfig(String key) {
     String prop = properties.getProperty(key);
-    if (prop.isBlank()) throw new RuntimeException("Config with key: " + key + " is not defined");
+    if (prop.isBlank())
+      throw new RuntimeException("Config with key: " + key + " is not defined");
     return prop;
   }
 }
