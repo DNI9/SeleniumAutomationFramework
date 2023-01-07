dcdbbe52b31a78dcb7f357e5ef9c73e8ac4997e5
DNI9 <itsr9boi@gmail.com>
2023-01-07 19:39:15 +0530

    take screenshot on test failure

--- src/test/java/org/dni9/pom/base/BaseTest.java
+++ src/test/java/org/dni9/pom/base/BaseTest.java
@@ -1,17 +1,23 @@
 package org.dni9.pom.base;
 
 import io.restassured.http.Cookies;
+import org.apache.commons.io.FileUtils;
 import org.dni9.pom.constants.DriverType;
 import org.dni9.pom.factory.DriverManager;
 import org.dni9.pom.factory.DriverManagerFactory;
 import org.dni9.pom.utils.CookieUtils;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class BaseTest {
@@ -36,12 +42,25 @@ public class BaseTest {
   // NOTE: by using `synchronized` keyword, other thread have to wait before one thread finishes using this method
   // basically making this run sequentially.
   @AfterMethod
-  public synchronized void quitDriver() {
+  @Parameters("browser")
+  public synchronized void quitDriver(@Optional("CHROME") String browser, ITestResult result) throws IOException {
+    if (result.getStatus() == ITestResult.FAILURE) {
+      String filePath = "screenshots" + File.separator + browser + File.separator
+          + result.getTestClass().getRealClass().getSimpleName() + "_"
+          + result.getMethod().getMethodName() + ".png";
+      File destFile = new File(filePath);
+      takeScreenshot(destFile);
+    }
     getDriver().quit();
   }
 
   public void injectCookiesToBrowser(Cookies cookies) {
     List<Cookie> seleniumCookies = new CookieUtils().convertToSeleniumCookies(cookies);
     seleniumCookies.forEach(cookie -> getDriver().manage().addCookie(cookie));
+  }
+
+  private void takeScreenshot(File destFile) throws IOException {
+    File srcFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+    FileUtils.copyFile(srcFile, destFile);
   }
 }
