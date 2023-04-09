668f5596eeb4fb8586b0bf125ac9cdbdf246ac23
DNI9 <itsr9boi@gmail.com>
2023-04-09 23:53:50 +0530

    add driver version to config

--- src/test/java/org/dni9/pom/factory/ChromeDriverManager.java
+++ src/test/java/org/dni9/pom/factory/ChromeDriverManager.java
@@ -1,17 +1,23 @@
 package org.dni9.pom.factory;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.dni9.pom.utils.ConfigLoader;
 import org.dni9.pom.utils.EnvUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeDriverManager implements DriverManager {
+
+  public static final String DRIVER_VERSION = ConfigLoader.getInstance().getConfig("chrome.driver.version");
+
   @Override
   public WebDriver createDriver() {
-    WebDriverManager.chromedriver().setup();
+    WebDriverManager.chromedriver().driverVersion(DRIVER_VERSION).setup();
 
     ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+
     if (EnvUtils.isCIServer()) {
       options.addArguments(
           "--headless",
