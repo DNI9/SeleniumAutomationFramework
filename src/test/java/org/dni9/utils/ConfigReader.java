5b17d10e4f049975b5dcc9eb6e72a267253db6ba
DNI9 <itsr9boi@gmail.com>
2025-01-05 15:26:37 +0530

    override with local config

--- src/test/java/org/dni9/utils/ConfigReader.java
+++ src/test/java/org/dni9/utils/ConfigReader.java
@@ -9,43 +9,53 @@ import java.util.Properties;
 @Slf4j
 public class ConfigReader {
 
-  private final Properties properties;
+    private final Properties properties;
 
-  public ConfigReader() {
-    properties = new Properties();
-    try (FileInputStream fis = new FileInputStream("src/test/resources/config.properties")) {
-      properties.load(fis);
-    } catch (IOException e) {
-      log.error("Failed to load config properties");
-      throw new RuntimeException(e);
+    public ConfigReader() {
+        properties = new Properties();
+        try (FileInputStream fis = new FileInputStream("src/test/resources/config.properties")) {
+            properties.load(fis);
+        } catch (IOException e) {
+            log.error("Failed to load config properties");
+            throw new RuntimeException(e);
+        }
+
+        // Override with local properties
+        try (FileInputStream fis = new FileInputStream("src/test/resources/local.properties")) {
+            Properties localProperties = new Properties();
+            localProperties.load(fis);
+            properties.putAll(localProperties); // override
+        } catch (IOException e) {
+            log.error("Failed to load local properties");
+            throw new RuntimeException(e);
+        }
     }
-  }
 
-  public String getProperty(String key) {
-    return getProperty(key, null);
-  }
+    public String getProperty(String key) {
+        return getProperty(key, null);
+    }
 
-  public String getProperty(String key, String defaultValue) {
-    return properties.getProperty(key, defaultValue);
-  }
+    public String getProperty(String key, String defaultValue) {
+        return properties.getProperty(key, defaultValue);
+    }
 
-  public String getBrowser() {
-    return properties.getProperty("browser");
-  }
+    public String getBrowser() {
+        return properties.getProperty("browser");
+    }
 
-  public boolean isHeadless() {
-    return Boolean.parseBoolean(properties.getProperty("headless"));
-  }
+    public boolean isHeadless() {
+        return Boolean.parseBoolean(properties.getProperty("headless"));
+    }
 
-  public int getImplicitWait() {
-    return Integer.parseInt(getProperty("implicitWait", "10"));
-  }
+    public int getImplicitWait() {
+        return Integer.parseInt(getProperty("implicitWait", "10"));
+    }
 
-  public boolean shouldReconnectSession() {
-    return "true".equals(getProperty("reconnectSession"));
-  }
+    public boolean shouldReconnectSession() {
+        return "true".equals(getProperty("reconnectSession"));
+    }
 
-  public boolean shouldSaveSession() {
-    return "true".equals(getProperty("saveSession"));
-  }
+    public boolean shouldSaveSession() {
+        return "true".equals(getProperty("saveSession"));
+    }
 }
